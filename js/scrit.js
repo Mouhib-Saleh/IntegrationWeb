8193d05b3f0167c70bbf05f0f19f281941325e51
Mouhib-Saleh <mouhib.saleh20@gmail.com>
2023-05-27 18:08:55 +0100

    first commit

---
+++ js/scrit.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+   
+    const burgerToggle = document.querySelector('.burger-toggle');
+
+    burgerToggle.classList.toggle('active');
+      const nav = document.querySelector('nav');
+      nav.classList.toggle('active');
+  }
